09a3be31b047f5c85c8ea3039d2277652c4ef252
sajjannehal <sajjansingh72@yahoo.com>
2018-08-30 20:01:57 +0530

    startup

---
+++ dojo/home/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from dojo.home import views
+
+urlpatterns = [
+    #  dojo home pages
+    url(r'^index', views.index, name='index'),
+    url(r'^$', views.home, name='home'),
+    url(r'^dashboard$', views.dashboard, name='dashboard'),
+]
